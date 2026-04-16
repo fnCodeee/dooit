8ae821255c64c35d13dca8410c7ec6f3790e566c
fnCodeee <faizna2213@gmail.com>
2026-04-16 17:20:40 +0700

    add(modal): task modal category icons

---
+++ public/icons/Icons.js
@@ -0,0 +1,11 @@
+// Awali dengan bx bx-${icons}
+const Icons = [
+  "dumbbell-alt",
+  "book-open",
+  "briefcase-alt-2",
+  "popcorn",
+  "fork-spoon",
+  "fork-spoon",
+  "dollar-circle",
+  "globe-alt",
+];
